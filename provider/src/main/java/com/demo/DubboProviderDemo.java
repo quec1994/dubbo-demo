e58495a61c25d481837fb38a04aeb8a5c611a4bc
qyz <qyz12321@163.com>
2023-07-15 00:52:02 +0800

    更正项目配置结构

--- provider/src/main/java/com/demo/DubboProviderDemo.java
+++ provider/src/main/java/com/demo/DubboProviderDemo.java
@@ -1,11 +1,9 @@
 package com.demo;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDubbo(scanBasePackages = "com.demo.provider.service")
 public class DubboProviderDemo {
 
     public static void main(String[] args) {
