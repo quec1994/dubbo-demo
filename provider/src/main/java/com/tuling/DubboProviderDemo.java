7f2f9651c746c922193fd326abff005c8e8e8443
qyz <qyz12321@163.com>
2023-07-14 23:51:56 +0800

    使用注解的方式启动Dubbo

--- provider/src/main/java/com/tuling/DubboProviderDemo.java
+++ provider/src/main/java/com/tuling/DubboProviderDemo.java
@@ -1,12 +1,15 @@
 package com.tuling;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo(scanBasePackages = "com.tuling.provider.service")
 public class DubboProviderDemo {
 
     public static void main(String[] args) {
         SpringApplication.run(DubboProviderDemo.class, args);
     }
+
 }
