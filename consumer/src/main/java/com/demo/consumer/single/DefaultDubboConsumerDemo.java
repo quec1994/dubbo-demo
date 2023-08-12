9c91b8d5a3143cb304737e238957c06dcfbdb66b
qyz <qyz12321@163.com>
2023-08-12 16:46:54 +0800

    增加default的单项demo

---
+++ consumer/src/main/java/com/demo/consumer/single/DefaultDubboConsumerDemo.java
@@ -0,0 +1,20 @@
+package com.demo.consumer.single;
+
+import com.demo.DemoService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@EnableAutoConfiguration
+public class DefaultDubboConsumerDemo {
+
+    @DubboReference(version = "default")
+    private DemoService demoService;
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SingleDubboDemoStarter.run(DefaultDubboConsumerDemo.class);
+        DemoService demoService = context.getBean(DemoService.class);
+        System.out.println((demoService.sayHello("World")));
+    }
+
+}
