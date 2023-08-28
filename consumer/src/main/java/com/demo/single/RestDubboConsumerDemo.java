6d2f1fdc110391fa1d32fcc1917a123800e9c656
qyz <qyz12321@163.com>
2023-08-28 10:49:30 +0800

    http请求优化

--- consumer/src/main/java/com/demo/single/RestDubboConsumerDemo.java
+++ consumer/src/main/java/com/demo/single/RestDubboConsumerDemo.java
@@ -2,56 +2,41 @@ package com.demo.single;
 
 import com.demo.dubbo.RestDemoService;
 import com.demo.single.starter.SingleDubboConsumerDemoStarter;
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.rpc.protocol.rest.RestHeaderEnum;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @EnableAutoConfiguration
+@Import(RestTemplate.class)
 public class RestDubboConsumerDemo {
 
     @DubboReference(version = "rest")
     private RestDemoService restDemoService;
 
     public static void main(String[] args) {
-        dubboDemo();
-//        httpDemo();
-    }
-
-    private static void dubboDemo() {
         ConfigurableApplicationContext context = SingleDubboConsumerDemoStarter.run(RestDubboConsumerDemo.class);
+
+        // dubboDemo
         RestDemoService restDemoService = context.getBean(RestDemoService.class);
         System.out.println("dubboDemo：" + restDemoService.sayHello("World"));
-    }
 
-    private static void httpDemo() {
-        String spec = "http://localhost:10880/demo/say?name=World";
-        System.out.println("httpDemo：" + HttpClient.get(spec));
-    }
-
-    public static class HttpClient {
-
-        public static String get(String url) {
-            OkHttpClient okHttpClient = new OkHttpClient();
-            final Request request = new Request.Builder()
-                    .url(url)
-                    .build();
-            final Call call = okHttpClient.newCall(request);
-            try (Response response = call.execute()) {
-                if (response.body() != null) {
-                    return response.body().string();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
+        // httpDemo
+        RequestEntity<Void> requestEntity = RequestEntity
+                .get("http://localhost:10880/demo/say?name=World")
+                .header(RestHeaderEnum.VERSION.getHeader(), "rest")
+//                .header(RestHeaderEnum.GROUP.getHeader(), null)
+                .build();
+        RestTemplate restTemplate = context.getBean(RestTemplate.class);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
+        String result = new String(responseEntity.getBody().getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+        System.out.println("httpDemo：" + result);
     }
 
 }
