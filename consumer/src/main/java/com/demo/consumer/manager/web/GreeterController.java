fa84513c2b57edd444a0100336c729eb7854255d
qyz <qyz12321@163.com>
2023-09-01 08:55:44 +0800

    更正类名

--- consumer/src/main/java/com/demo/consumer/manager/web/GreeterController.java
+++ consumer/src/main/java/com/demo/consumer/manager/web/GreeterController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("salute")
-public class SaluteController {
+@RequestMapping("greeter")
+public class GreeterController {
 
     @Autowired
     private DemoService demoService;
