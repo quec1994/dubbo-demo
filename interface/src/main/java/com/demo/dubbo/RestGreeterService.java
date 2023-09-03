c6e204138f620df8d60d84dc0085bf6de9297554
qyz <qyz12321@163.com>
2023-09-03 16:49:55 +0800

    REST协议demo优化

--- interface/src/main/java/com/demo/dubbo/RestGreeterService.java
+++ interface/src/main/java/com/demo/dubbo/RestGreeterService.java
@@ -1,10 +1,7 @@
 package com.demo.dubbo;
 
-import com.demo.util.ContentType;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
 @Path("demo")
@@ -12,7 +9,6 @@ public interface RestGreeterService {
 
     @GET
     @Path("say")
-    @Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
     String sayHello(@QueryParam("name") String name);
 
 }
