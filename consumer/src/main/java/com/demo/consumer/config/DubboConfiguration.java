9542a4c2689b2437a677a670cb102367e387d32d
qyz <qyz12321@163.com>
2023-09-08 16:38:08 +0800

    优化dubbo日志工厂指定的地方

--- consumer/src/main/java/com/demo/consumer/config/DubboConfiguration.java
+++ consumer/src/main/java/com/demo/consumer/config/DubboConfiguration.java
@@ -1,6 +1,7 @@
 package com.demo.consumer.config;
 
 import com.demo.dubbo.GreeterService;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,24 @@ public class DubboConfiguration {
 
     public static final String CONSUMER_PROPERTIES = "classpath:/dubbo/dubbo-consumer.yml";
 
+    static {
+        forceAssignLoggerFactory();
+    }
+
     @DubboReference(group ="default")
     private GreeterService greeterService;
+
+    private static void forceAssignLoggerFactory() {
+        /*
+         * 强制指定Dubbo框架使用SLF4J打印日志，在Dubbo框架的Logger工厂里log4j的优先级比slf4j高，
+         * 如果项目中存在log4j的包的话，Dubbo框架的日志会打印不出来，
+         * dubbo框架的Logger工厂类：org.apache.dubbo.common.logger.LoggerFactory
+         */
+        String loggerKey = "dubbo.application.logger";
+        String logger = System.getProperty(loggerKey);
+        if (StringUtils.isBlank(logger)) {
+            System.setProperty(loggerKey, "slf4j");
+        }
+    }
 
 }
--- provider/src/main/java/com/demo/provider/DubboProviderDemo.java
+++ provider/src/main/java/com/demo/provider/DubboProviderDemo.java
@@ -6,15 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DubboProviderDemo {
 
-    static {
-        /*
-         * 强制指定Dubbo框架使用SLF4J打印日志，在Dubbo框架的Logger工厂里log4j的优先级比slf4j高，
-         * 如果项目中存在log4j的包的话，Dubbo框架的日志会打印不出来，
-         * dubbo框架的Logger工厂类：org.apache.dubbo.common.logger.LoggerFactory
-         */
-        System.setProperty("dubbo.application.logger", "slf4j");
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(DubboProviderDemo.class, args);
     }
