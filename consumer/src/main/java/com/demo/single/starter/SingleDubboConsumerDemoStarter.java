81d1021663a40e8ed72bde6b5c30f86eb302f3ed
qyz <qyz12321@163.com>
2023-09-10 10:31:45 +0800

    dubbo启动配置优化

--- consumer/src/main/java/com/demo/single/starter/SingleDubboConsumerDemoStarter.java
+++ consumer/src/main/java/com/demo/single/starter/SingleDubboConsumerDemoStarter.java
@@ -12,8 +12,6 @@ public class SingleDubboConsumerDemoStarter {
 
     static {
         System.setProperty("dubbo.application.logger", "slf4j");
-        // 只注册应用级地址，让注册中心干净一点
-        System.setProperty("dubbo.application.register-mode", "instance");
     }
 
     public static ConfigurableApplicationContext run(Class<?>... primarySourceClasses) {
